fdd2b22801f77ac6d26b40869840107cee0a6759
Douglas Desiderio <38764223+Douglas16@users.noreply.github.com>
2023-06-28 20:38:03 +0000

    Adicionando Prettier `prettier` .

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Implementação tabnews 
+
+Implementação tabnews
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Estamos sempre juntos, mesmo entre telas, Te amo!!! ❤ </h1>
+  return <h1>Estamos sempre juntos, mesmo entre telas, Te amo!!! ❤ </h1>;
 }
 
 export default Home;
